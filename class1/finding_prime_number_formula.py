a04c6bdb65ba3d69ff5a95e89d65a19ebeebcb74
MaviaZindani <maviazindaninfo@gmail.com>
2025-06-01 06:29:05 -0700

    secound commit

--- class1/finding_prime_number_formula.py
+++ class1/finding_prime_number_formula.py
@@ -1,5 +1,20 @@
 # Finding prime numbers
 
-number = int(input('Give Number To Check it is prime or not: '))
+def is_prime_recursive(n, i=2):
+    if n <= 1:
+        return False
+    if i * i > n:
+        return True
+    if n % i == 0:
+        return False
+    return is_prime_recursive(n, i + 1)
 
-divedNumber = number / number
+# Example
+number = int(input('give a number: '))
+
+isItPrimeNumber = is_prime_recursive(number)
+
+if isItPrimeNumber:
+    print('This Is A Prime Number')
+else :
+    print('This Is A Not Prime Number')
